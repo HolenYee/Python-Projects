db6559e49f764e5dc0ed466f302955b18543eaff
HolenYee <71401943+HolenYee@users.noreply.github.com>
2020-12-22 10:28:43 -0500

    Update TwitterBot.py

--- TwitterBot.py
+++ TwitterBot.py
@@ -1,4 +1,5 @@
 import tweepy
+import time
 
 consumer_key = "VJuYBEB9R9pNLjbYsrujbfBw4"
 consumer_secret = "REszrc9Zv5dGLffvc8zsxNW19FDxO3JjHQKvmS512cE7Ntr4ZG"
@@ -8,7 +9,20 @@ secret = "RAAXF7LOE46zLnJArFAmRXHxX7wIfU7TqKJnlYToFKesJ"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
 api = tweepy.API(auth)
-for result in api.search("#theaveragejoe"):
-    # print(result.entities["hashtags"][0]["text"])
-    api.retweet(result.id_str)
 
+hashtag = "#theaveragejoe"
+tweetNumber = 10
+
+tweets = tweepy.Cursor(api.search, hashtag).pages(tweetNumber)
+
+def retweetbot():
+    for tweet in tweets:
+        try:
+            api.retweet(tweet[0].id)
+            print("Retweet done")
+            time.sleep(5)
+        except tweepy.TweepError as e:
+            print(e.reason)
+            time.sleep(5)
+
+retweetbot()
